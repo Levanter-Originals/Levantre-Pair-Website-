f7d9c42c3789268bdb57eac97c0d58307d14b8e1
Levanter-Originals <levanteroriginals@gmail.com>
2025-09-08 18:57:44 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: '',
-    password: '',
+    email: 'levanterbass@gmail.com',
+    password: 'E3VZz_a5.8TRFmT',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
